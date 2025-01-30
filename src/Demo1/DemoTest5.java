92d34b365e2dc9c6bff8d60ea0052ff43e67b362
zaidalramadhani <zaidalramadhani@localhost>
2025-01-29 20:52:48 -0500

    Implemented logic to add 2 variables

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -3,5 +3,13 @@ package Demo1;
 public class DemoTest5 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		int a = 10; 
+		int b = 20; 
+		
+		
+		int result = a + b; 
+		
+		System.out.println(result);
 	}
 }
