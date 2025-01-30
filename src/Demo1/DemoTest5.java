d3db0c2348e6414382c9c7a18db892e17e119104
zaidalramadhani <zaidalramadhani@localhost>
2025-01-29 20:58:20 -0500

    Implemented add()

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -2,7 +2,7 @@ package Demo1;
 
 public class DemoTest5 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("Batch13: GIT LECTURE");
 		
 		int a = 10; 
 		int b = 20; 
@@ -11,5 +11,10 @@ public class DemoTest5 {
 		int result = a + b; 
 		
 		System.out.println(result);
+	}
+	
+	
+	public void add (int a, int b) { 
+		System.out.println(a + b);
 	}
 }
